fadd8844fa4167268b56acf60a666e11312dcb07
KshitizAnand123 <kshitizanand2024@gmail.com>
2026-03-11 14:56:29 +0530

    Deploying the code of project

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import cv2
+import os
+from cartoonify import cartoonify_image
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "static"
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+
+        file = request.files["image"]
+
+        original_path = os.path.join("static", "original.jpg")
+        file.save(original_path)
+
+        # FIX 1
+        cartoon = cartoonify_image(original_path)
+
+        output_path = os.path.join(
+            OUTPUT_FOLDER,
+            "cartoon.jpg"
+        )
+
+        cv2.imwrite(output_path, cv2.cvtColor(cartoon, cv2.COLOR_RGB2BGR))
+
+        return render_template(
+            "index.html",
+            cartoon="cartoon.jpg",
+            original="original.jpg"   # FIX 2
+        )
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
---
+++ cartoonify.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+def edge_mask(img, line_size, blur_value):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray_blur = cv2.medianBlur(gray, blur_value)
+
+    edges = cv2.adaptiveThreshold(
+        gray_blur,
+        255,
+        cv2.ADAPTIVE_THRESH_MEAN_C,
+        cv2.THRESH_BINARY,
+        line_size,
+        blur_value
+    )
+
+    return edges
+
+
+def color_quantization(img, k):
+
+    data = np.float32(img).reshape((-1,3))
+
+    criteria = (
+        cv2.TERM_CRITERIA_EPS +
+        cv2.TERM_CRITERIA_MAX_ITER,
+        20,
+        0.001
+    )
+
+    ret, label, center = cv2.kmeans(
+        data, k, None, criteria, 10,
+        cv2.KMEANS_RANDOM_CENTERS
+    )
+
+    center = np.uint8(center)
+
+    result = center[label.flatten()]
+    result = result.reshape(img.shape)
+
+    return result
+
+
+def cartoonify_image(path):
+
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    edges = edge_mask(img, 7, 7)
+
+    quantized = color_quantization(img, 8)
+
+    blurred = cv2.bilateralFilter(
+        quantized,
+        d=7,
+        sigmaColor=200,
+        sigmaSpace=200
+    )
+
+    cartoon = cv2.bitwise_and(
+        blurred,
+        blurred,
+        mask=edges
+    )
+
+    return cartoon
